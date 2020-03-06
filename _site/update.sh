7e91a7721452799162329a27a435dfbcb908fc44
Weihua Hu <weihua916@gmail.com>
2020-03-05 19:52:18 -0800

    update page

--- _site/update.sh
+++
@@ -1,6 +0,0 @@
-python update_leaderboard.py 
-git add _docs/leaderboard/leader_graphprop.md
-git add _docs/leaderboard/leader_linkprop.md
-git add _docs/leaderboard/leader_nodeprop.md
-git commit -m updateleaderboard
-git push origin master
