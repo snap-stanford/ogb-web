f203f58222e3cdd4d07f54082b82e19ca51f222a
Weihua Hu <weihua916@gmail.com>
2020-03-21 00:26:33 -0700

    update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 python update_leaderboard.py 
-git add _docs/leaderboard/leader_graphprop.md
-git add _docs/leaderboard/leader_linkprop.md
-git add _docs/leaderboard/leader_nodeprop.md
+git add _docs/leaderboard/*.md
 git commit -m updateleaderboard
 git push origin master
