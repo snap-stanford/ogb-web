42cf0cd0d97ff4f1fbc661f35cf49c5ad9b4c5ac
Weihua Hu <weihua916@gmail.com>
2022-02-03 09:40:35 -0800

    update update_lsc script

--- update_lsc.sh
+++ update_lsc.sh
@@ -1,2 +1,7 @@
 python merge_lsc_leaderboard.py 
 python update_lsc_leaderboard.py 
+git add _docs/lsc/leaderboard.md
+git add test-dev_master.csv
+git add test-dev_leaderboard.csv
+git commit -m updatelscleaderboard
+git push origin master
