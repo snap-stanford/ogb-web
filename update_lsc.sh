e0ecd5110a1fafee2825ac3190bc679e2d43a1d0
Weihua Hu <weihua916@gmail.com>
2022-02-03 09:49:24 -0800

    fix typo in update_lsc script

--- update_lsc.sh
+++ update_lsc.sh
@@ -1,6 +1,6 @@
 python merge_lsc_leaderboard.py 
 python update_lsc_leaderboard.py 
-git add _docs/lsc/leaderboard.md
+git add _docs/lsc/leaderboards.md
 git add test-dev_master.csv
 git add test-dev_leaderboard.csv
 git commit -m updatelscleaderboard
